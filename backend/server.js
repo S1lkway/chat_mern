d685e1c0b8b454574db30e1767a69bb040bd9548
Artem <silkwayway@gmail.com>
2023-08-05 10:35:20 -0400

    add connection to MongoDB and error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,29 @@ const path = require('path');
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-// const connectDB = require('./config/db')
-// const { errorHandler } = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
+const app = express()
+
+
+// //* PATH TO GET IMAGES FROM BACKEND TO FRONTEND
+// /* We make absolute path to folder with files */
+// const articleUploadsPath = path.join(__dirname, 'uploads', 'articleUploads');
+// /* Set path on frontend that will use backend path in articleUploadsPath */
+// app.use('/uploads/articleUploads', express.static(articleUploadsPath));
+
+
+//* MIDDLEWARES
+/* Used to parse incoming requests with JSON payloads */
+app.use(express.json())
+/* Used to parse incoming requests with URL-encoded payloads 
+The extended option determines how the URL-encoded data is parsed. When set to false, the data is parsed using the querystring*/
+app.use(express.urlencoded({ extended: false }))
+
+/* Provides a basic error handling mechanism in Express that sends a JSON response with an error message and, optionally, the stack trace */
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`.yellow.underline))
 
