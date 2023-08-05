eec57310f0ffb1821e68b4051824d200ddc6cd51
Artem <silkwayway@gmail.com>
2023-08-05 10:12:30 -0400

    add basic dependencies for backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path');
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+// const connectDB = require('./config/db')
+// const { errorHandler } = require('./middleware/errorMiddleware')
+
