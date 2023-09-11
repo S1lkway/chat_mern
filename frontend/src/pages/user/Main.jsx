f544ffe05a5f183f7ca166619dc39e74414ecf55
Artem <silkwayway@gmail.com>
2023-09-11 09:20:57 -0400

    components background

--- frontend/src/pages/user/Main.jsx
+++ frontend/src/pages/user/Main.jsx
@@ -3,21 +3,23 @@ function Main() {
     <div className="content grid_row">
       <img className="mainPicture" src="../../images/banner-groups.jpg" alt="Girl in a jacket"></img>
       <section className='heading'>
-        <h3>
-          <p>
-            1. Create account
-          </p>
-        </h3>
-        <h3>
-          <p>
-            2. Find your friends
-          </p>
-        </h3>
-        <h3>
-          <p>
-            3. Connect and start chatting!
-          </p>
-        </h3>
+        <i>
+          <h3>
+            <p>
+              1. Create account
+            </p>
+          </h3>
+          <h3>
+            <p>
+              2. Find your friends
+            </p>
+          </h3>
+          <h3>
+            <p>
+              3. Connect and start chatting!
+            </p>
+          </h3>
+        </i>
       </section>
     </div>
   )
