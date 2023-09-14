d9340324ab3b50aef96dd0f6ece2df29c8c6572a
Artem <silkwayway@gmail.com>
2023-09-14 11:19:33 -0400

    messages style

--- frontend/src/pages/chat/Messages.jsx
+++ frontend/src/pages/chat/Messages.jsx
@@ -4,6 +4,31 @@ function Messages() {
   return (
     <>
       <div className="messages_area">
+        <div className="message_box">
+          <div className="message">
+            <span className="message_text">Modi, repudiandae soluta maiores facere ad molestias corrupti error</span>
+          </div>
+        </div>
+        <div className="message_box flex_end">
+          <div className="message">
+            <span className="message_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aspernatur voluptate voluptatum enim, sapiente magnam eaque ratione. Modi, repudiandae soluta maiores facere ad molestias corrupti error est officiis dolore ratione.</span>
+          </div>
+        </div>
+        <div className="message_box">
+          <div className="message">
+            <span className="message_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+          </div>
+        </div>
+        <div className="message_box">
+          <div className="message">
+            <span className="message_text">Modi, repudiandae soluta maiores facere ad molestias corrupti error est officiis dolore ratione.</span>
+          </div>
+        </div>
+        <div className="message_box flex_end">
+          <div className="message">
+            <span className="message_text">Lorem</span>
+          </div>
+        </div>
       </div>
       <AddMessageBar />
     </>
