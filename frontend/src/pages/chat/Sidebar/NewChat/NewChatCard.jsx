c76eee092baa21aa1fdb237affd75c5ca6633c71
Artem <silkwayway@gmail.com>
2023-10-05 10:14:30 -0400

    confirmation modal window

--- frontend/src/pages/chat/Sidebar/NewChat/NewChatCard.jsx
+++ frontend/src/pages/chat/Sidebar/NewChat/NewChatCard.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { FaUserPlus } from "react-icons/fa";
+import ReactModal from 'react-modal';
 // - Redux
 import { createChat } from "../../../../features/chats/chatsSlice";
 
 function NewChatCard(props) {
   const dispatch = useDispatch()
   const userData = props.userData
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const createNewChat = (id) => {
+  //*DELETE ARTICLE IN MODAL
+  const openCreateModal = () => {
+    setModalIsOpen(true);
+  };
+  const closeCreateModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const createNewChat = () => {
     // console.log('Create chat with user ' + id)
-    const chatData = { id: id }
+    const chatData = { id: userData._id }
     dispatch(createChat(chatData))
+    closeCreateModal();
   }
   return (
     <div className="card" title="Open chat">
@@ -18,7 +30,22 @@ function NewChatCard(props) {
         <span className="card_name">{userData.name}</span>
         <span className="card_email"><i>{userData.email}</i></span>
       </div>
-      <div className="delete-button" title="Create chat" onClick={() => createNewChat(userData._id)}><FaUserPlus /></div>
+      <div className="delete-button" title="Create chat" onClick={openCreateModal}>
+        <FaUserPlus />
+      </div>
+
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeCreateModal}
+        className="confirmationModal"
+        overlayClassName="confirmationOverlay"
+      >
+        <h3>Are you sure?</h3>
+        <div className='modalButtons'>
+          <button onClick={createNewChat} className='btn'>Yes</button>
+          <button onClick={closeCreateModal} className='btn'>No</button>
+        </div>
+      </ReactModal>
     </div>
   )
 }
