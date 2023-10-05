44cce5d283c645f9ad2bd60caf5b5be11c1e1299
Artem <silkwayway@gmail.com>
2023-10-05 09:50:49 -0400

    install react-modal

--- frontend/src/pages/chat/Sidebar/NewChat/UserCards.jsx
+++ frontend/src/pages/chat/Sidebar/NewChat/UserCards.jsx
@@ -13,6 +13,7 @@ function UserCard(props) {
       ) : (
         <div className="heading newChatsCards">
           <p>Contacts for chats were not found</p>
+          <span><i>This is the test app. Search "com" to find all available contacts in DataBase</i></span>
 
         </div>
       )}
