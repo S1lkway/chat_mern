4af7a95694a76e49488a25572164790a8bd2b6e4
Artem <silkwayway@gmail.com>
2023-10-10 10:34:22 -0400

    add instructions for chat search

--- frontend/src/pages/chat/Sidebar/NewChat/UserCards.jsx
+++ frontend/src/pages/chat/Sidebar/NewChat/UserCards.jsx
@@ -13,7 +13,7 @@ function UserCard() {
       ) : (
         <div className="heading newChatsCards">
           <p>Contacts for chats were not found</p>
-          <span><i>This is the test app. Search "com" to find all available contacts in DataBase</i></span>
+          <span><i>This is the test app. Search "com" to find all available contacts in DataBase. You can register multiple accounts to search for a new chat.</i></span>
 
         </div>
       )}
